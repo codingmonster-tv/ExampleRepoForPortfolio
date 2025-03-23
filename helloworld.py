c829a9d32c5219607649de3faafa0e4e43551ba3
dongyi <dongyi@MacBook-Pro-103.local>
2025-03-23 22:51:44 +0900

    refactor: use f-string

--- helloworld.py
+++ helloworld.py
@@ -4,4 +4,4 @@ This is a simple Python script that prints "Hello, World!" to the console.
 
 if __name__ == '__main__':
     user_name = input('What is your name? ')
-    print('Hello ' + user_name + '!')
+    print(f'Hello {user_name}!')
